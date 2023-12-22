b9a80d9318bc51148f905266cc1440eda6033c13
yelo-o <flyordig@gmail.com>
2023-12-22 23:15:38 +0900

    백준 문제 풀이

---
+++ etc/December/december21/Boj10250.java
@@ -0,0 +1,21 @@
+package etc.December.december21;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Boj10250 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        for (int i = 0; i < count; i++) {
+            int h = sc.nextInt();
+            int w = sc.nextInt();
+            int n = sc.nextInt();
+            if (n % h == 0) {
+                System.out.println((h * 100) + (n / h));
+            } else {
+                System.out.println(((n % h) * 100) + ((n / h) + 1));
+            }
+        }
+    }
+}
