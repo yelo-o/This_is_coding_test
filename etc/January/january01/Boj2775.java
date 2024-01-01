aead95a24af1b0f103985698d91002e6ba4701aa
yelo-o <flyordig@gmail.com>
2024-01-01 22:54:12 +0900

    반상회 문제 푸는중

---
+++ etc/January/january01/Boj2775.java
@@ -0,0 +1,25 @@
+package etc.January.january01;
+
+import java.util.Scanner;
+
+public class Boj2775 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt(); // 테스트 케이스
+        for (int i = 0; i < T; i++) {
+            int k = sc.nextInt(); // 층
+            int n = sc.nextInt(); // 호
+//            int answer = getNumber(k, n);
+//            System.out.println(answer);
+        }
+
+    }
+
+//    private static int getNumber(int k, int n) {
+//        if () {
+//            return
+//        } else {
+//            return getNumber()
+//        }
+//    }
+}
