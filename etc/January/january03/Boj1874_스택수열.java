ae46f71de943725c8c04aefc3f6c0061a1e67517
yelo-o <flyordig@gmail.com>
2024-01-05 23:30:56 +0900

    큐

---
+++ etc/January/january03/Boj1874_스택수열.java
@@ -0,0 +1,43 @@
+package etc.January.january03;
+
+import java.io.*;
+import java.util.Stack;
+
+public class Boj1874_스택수열 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Stack<Integer> stack = new Stack<>();
+        int number = 1;
+        boolean flag = true;
+        for (int i = 0; i < n; i++) {
+            int curNum = arr[i]; // 현재 수열 값
+            if (curNum >= number) {
+                while (curNum >= number) {
+                    stack.push(number);
+                    number++;
+                    sb.append("+\n");
+                }
+                stack.pop();
+                sb.append("-\n");
+            } else {
+                int top = stack.pop();
+                if (top > curNum) {
+                    System.out.println("NO");
+                    flag = false;
+                    break;
+                } else {
+                    sb.append("-\n");
+                }
+            }
+        }
+        if (flag) {
+            System.out.println(sb);
+        }
+    }
+}
