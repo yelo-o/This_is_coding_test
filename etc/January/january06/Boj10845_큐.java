b81dffda5aaee49b400f80df5655f187974ae26b
yelo-o <flyordig@gmail.com>
2024-01-06 23:55:56 +0900

    linkedlist를 이용한 큐 자료구조 구현시작

---
+++ etc/January/january06/Boj10845_큐.java
@@ -0,0 +1,16 @@
+package etc.January.january06;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Boj10845_큐 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Queue<Integer> que = new LinkedList<>();
+
+    }
+}
